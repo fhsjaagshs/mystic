f7a7ede5c2fdc43a58d09c391579cdce2819b770
Nathaniel Symer <nate@natesymer.com>
2014-04-20 22:23:51 -0400

    Fixed typos, style cleanup

--- lib/mystic/model.rb
+++ lib/mystic/model.rb
@@ -16,22 +16,21 @@ module Mystic
       count = opts.delete(:count) || 0
       pairs = opts.sqlize
       
-      sql = "SELECT * FROM #{self.table_name}"
+      sql = "SELECT #{self.table_name}.* FROM #{self.table_name}"
       sql << " WHERE #{pairs.join(" AND ")}" if pairs.count > 0
       sql << " LIMIT #{count.to_i.to_s}" if count > 0
       sql
     end
     
     def self.function_sql(funcname, *params)
-      params.map!{ |param| "'" + param.to_s.sanitize + "'" }
-      "SELECT " + funcname.to_s "(" + params.join(",") + ");"
+      "SELECT " + funcname.to_s "(" + params.map{ |param| "'" + param.to_s.sanitize + "'" }.join(",") + ");"
     end
     
     def self.update_sql(where={}, set={})
       where_pairs = where.sqlize
       set_pairs = set.sqlize
       return nil if where_pairs.count == 0
-      return nil if set_pairs.count == 08
+      return nil if set_pairs.count == 0
       
       "UPDATE " + self.table_name + " SET " + set_pairs.join(",") + " WHERE " + where_pairs.join(" AND ")
     end
@@ -44,7 +43,6 @@ module Mystic
     def self.delete_sql(opts={})
       where = opts.sqlize
       return nil if where.length == 0
-      
       "DELETE FROM " + self.table_name + " WHERE " + where.join(" AND ")
     end
     
