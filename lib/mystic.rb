018c48856802489885b5e0ac16a4e48df864a2bb
Nathaniel Symer <nate@natesymer.com>
2014-06-11 09:33:26 -0400

    Dry Cleanups

--- lib/mystic.rb
+++ lib/mystic.rb
@@ -38,7 +38,7 @@ module Mystic
 		
 		# get adapter name
 		adapter = db_conf.delete("adapter").to_s.downcase
-		adapter = "postgres" if adapter =~ /^postgr.*$/i # Intentionally includes PostGIS
+		adapter = "postgres" if adapter =~ /^postg.*$/i # Intentionally includes PostGIS
 		adapter = "mysql" if adapter =~ /^mysql.*$/i
 		
 		# setup our adapter
@@ -88,11 +88,18 @@ module Mystic
 	# Returns:
 	#   A pathname to the application's root
   def self.root(path=Pathname.new(Dir.pwd))
+		return @@root unless @@root.nil?
 		raise RootError, "Failed to find application's root." if path == path.parent
 		mystic_path = path.join "mystic"
 		return path if mystic_path.exist? && mystic_path.directory?
 		root(path.parent)
   end
+	
+	# Mystic.create_table
+	#   Create migration tracking table
+	def self.create_table
+		execute("CREATE TABLE IF NOT EXISTS mystic_migrations (mig_number integer, filename text)")
+	end
 	
 	#
 	# Command line
@@ -111,30 +118,31 @@ module Mystic
 	
   # Runs every yet-to-be-ran migration
 	def self.migrate
-		execute("CREATE TABLE IF NOT EXISTS mystic_migrations (mig_number integer, filename TEXT)")
+		create_table
 	  migrated_filenames = execute("SELECT filename FROM mystic_migrations").map{ |r| r["filename"] }
 	  mp = root.join("mystic","migrations").to_s
 		
 	  Dir.entries(mp)
-			.reject{ |e| MIG_REGEX.match(e).nil? || migrated_filenames.include?(e) }
+			.reject{ |e| MIG_REGEX.match(e).nil? }
+			.reject{ |e| migrated_filenames.include?(e) }
 			.sort{ |a,b| MIG_REGEX.match(a)[:num].to_i <=> MIG_REGEX.match(b)[:num].to_i }
-			.each{ |fname| 
-		    require File.join(mp,fname)
-    
+			.each{ |fname|
+		    load File.join(mp,fname)
+				
 				mig_num,mig_name = MIG_REGEX.match(fname).captures
-		
+				
 		    Object.const_get(mig_name).new.up
-		    execute("INSERT INTO mystic_migrations (mig_number, filename) VALUES(#{mig_num},'#{fname}')")
+		    execute("INSERT INTO mystic_migrations (mig_number, filename) VALUES (#{mig_num},'#{fname}')")
 			}
 	end
 	
   # Rolls back a single migration
 	def self.rollback
-		execute("CREATE TABLE IF NOT EXISTS mystic_migrations (mig_number integer, filename TEXT)")
+		create_table
 		fname = execute("SELECT filename FROM mystic_migrations ORDER BY mig_number DESC LIMIT 1")[0]["filename"] rescue nil
 		return if fname.nil?
 
-	  require root.join("mystic","migrations",fname).to_s
+	  load root.join("mystic","migrations",fname).to_s
 		
 		mig_num,mig_name = MIG_REGEX.match(fname).captures
 
@@ -152,14 +160,12 @@ module Mystic
     
 		mig_path = root.join "mystic","migrations"
 
-    numbers = Dir.entries(mig_path.to_s)
-		.map { |fname| MIG_REGEX.match(fname)[:num].to_i rescue nil }
-		.compact
+		numbers = Dir.entries(mig_path.to_s)
+			.map { |fname| MIG_REGEX.match(fname)[:num].to_i rescue -1 }
 
 		mig_num = numbers.max.to_i+1 rescue 1
-		mig_fname = "#{mig_num}_#{name}.rb"
 
-		File.open(mig_path.join(mig_fname).to_s, 'w') { |f| f.write(template(name)) }
+		File.open(mig_path.join("#{mig_num}_#{name}.rb").to_s, 'w') { |f| f.write(template(name)) }
 	end
 	
   # Retuns a blank migration's code in a String
