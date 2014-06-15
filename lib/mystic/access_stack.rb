e4b45c95b7188806bddf1997a5e6bfc1867ca001
GitMongo user <GitMongo email>
2014-06-15 18:05:30 -0400

    Rework thread safety

--- lib/mystic/access_stack.rb
+++ lib/mystic/access_stack.rb
@@ -21,42 +21,54 @@ module Mystic
 	class AccessStack
 		attr_reader :count
 		
+		TimeoutError = Class.new StandardError
+		
 		def initialize(opts={})
 			@timeout = opts[:timeout] || opts["timeout"] || -1
 			@size = opts[:size] || opts["size"] || 5
 			@stack = []
 			@count = 0
 			@mutex = Mutex.new
-			@cvar = ConditionVariable.new
 			@create_block = opts[:create] || opts["create"]
 			@destroy_block = opts[:destroy] || opts["destroy"]
 		end
 		
+		def threadsafe(timeout=-1,&block)
+			start_time = Time.now
+			while @mutex.locked?
+				sleep timeout/7
+				raise TimeoutError, "Took too long for the mutex to get a lock." if (Time.now-start_time).to_f >= timeout && timeout > 0
+			end
+			
+			@mutex.lock if @mutex.try_lock
+			return false unless @mutex.owned?
+			block.call	
+			@mutex.unlock
+			true
+		end
+				
 		def with(&block)
 			begin
 				obj = nil
-			
-				@mutex.synchronize do
-					@cvar.wait(@mutex) if @stack.count == 0 && @count > 0
+				
+				threadsafe @timeout do
 					obj = @stack.pop
 					if @count < @size && obj.nil?
 						@count += 1
 						obj = @create_block.call
 					end
-					@cvar.signal
 				end
 
 				return block.call obj
 			ensure
-				@mutex.synchronize do
+				threadsafe do
 					@stack.push obj
-					@cvar.signal # Should it be @cvar.broadcast???
 				end
 			end
 		end
 		
 		def empty
-			@mutex.synchronize do
+			threadsafe do
 				@stack.each { |instance| @destroy_block.call(instance) }
 				@stack.clear
 				@count = 0
@@ -67,13 +79,17 @@ module Mystic
 		# Returns: the number of objects created
 		def create(num=1)
 			created_count = 0
-			num.times do
-				if @count < @size
-					@count += 1
-					created_count += 1
-					@stack.push @create_block.call
+			
+			threadsafe do
+				num.times do
+					if @count < @size
+						@stack.push @create_block.call
+						@count += 1
+						created_count += 1
+					end
 				end
 			end
+				
 			created_count
 		end
 	end
