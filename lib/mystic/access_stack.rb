7fbbbbf276ab2d259a31fa78ceab61df9caa453b
Nathaniel Symer <nate@natesymer.com>
2014-06-15 22:58:04 -0400

    AccessStack: Better object validation

--- lib/mystic/access_stack.rb
+++ lib/mystic/access_stack.rb
@@ -12,21 +12,24 @@ AccessStack.new(
 	},
 	:destroy => lambda { |instance|
 		instance.close
+	},
+	:validate => lamda { |instance|
+		instance.status == CONNECTION_OK
 	}
 )
 =end
 
-# TODO: Expire objects
+# TODO: Expire objects on a timer
 
 module Mystic	
 	class AccessStack
 		attr_reader :count
-		attr_accessor :expires, :size, :create, :destroy
+		attr_accessor :expires, :size, :create, :destroy, :validate
 		
 		TimeoutError = Class.new StandardError
 		
 		def initialize(opts={})
-			@timeout = opts[:timeout] || opts["timeout"] || -1
+			@timeout = opts[:timeout] || opts["timeout"] || 5
 			@size = opts[:size] || opts["size"] || 5
 			@expires = opts[:expires] || opts["expires"] || -1
 			@expr_hash = {}
@@ -35,28 +38,9 @@ module Mystic
 			@mutex = Mutex.new
 			@create = opts[:create] || opts["create"]
 			@destroy = opts[:destroy] || opts["destroy"]
+			@validate = opts[:validate || opts["validate"]
 		end
 		
-		def threadsafe(timeout=-1,&block)
-			start_time = Time.now
-			while @mutex.locked?
-				sleep timeout/7 || 0.5
-				raise TimeoutError, "Took too long for the mutex to get a lock." if (Time.now-start_time).to_f >= timeout && timeout > 0
-			end
-			
-			@mutex.lock if @mutex.try_lock
-			return false unless @mutex.owned?
-			block.call	
-			@mutex.unlock
-			true
-		end
-		
-		def create_obj
-			obj = @create.call
-			@expr_hash[obj] = Time.now
-			obj
-		end
-				
 		def with(&block)
 			begin
 				obj = nil
@@ -64,7 +48,7 @@ module Mystic
 				threadsafe @timeout do
 					obj = @stack.pop
 					
-					if @expr_hash[obj]-Time.now > @expires
+					if !obj_valid obj && 
 						obj = nil
 						@count -= 1
 					end
@@ -84,6 +68,7 @@ module Mystic
 		end
 		
 		def empty
+			return if @count == 0
 			threadsafe @timeout do
 				@stack.each { |instance| @destroy.call instance }
 				@expr_hash.clear
@@ -92,8 +77,19 @@ module Mystic
 			end
 		end
 		
-		# Creates #{num} objects and adds them to the stack
-		# Returns: the number of objects created
+		
+		def reap
+			return true if @count == 0
+			threadsafe @timeout do
+				@stack.reject(&method(:obj_valid)).each do |instance|
+					@destroy.call instance
+					@expr_hash.delete instance
+					@stack.delete instance
+					@count -= 1
+				end
+			end
+		end
+
 		def create(num=1)
 			created_count = 0
 			
@@ -108,6 +104,39 @@ module Mystic
 			end
 				
 			created_count
+		end
+		
+		def empty?
+			@count == 0
+		end
+		
+		private
+		
+		def threadsafe(timeout=-1,&block)
+			start_time = Time.now
+			while @mutex.locked?
+				sleep timeout/10 if timeout > 0
+				sleep 0.5 if timeout == -1
+				raise TimeoutError, "Took too long for the mutex to get a lock." if (Time.now-start_time).to_f >= timeout && timeout > 0
+			end
+			
+			@mutex.lock if @mutex.try_lock
+			return false unless @mutex.owned?
+			block.call	
+			@mutex.unlock
+			true
+		end
+		
+		def create_obj
+			obj = @create.call
+			@expr_hash[obj] = Time.now
+			obj
+		end
+		
+		def obj_valid(obj)
+			block_valid = @vaildate.call obj rescue true
+			expired = (@expr_hash[obj]-Time.now > @expires && @expires > 0)
+			!expired && block_valid
 		end
 	end
 end
