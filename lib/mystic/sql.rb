26f582ef6fe4c1c013805cda43b80dc660b681c3
Nathaniel Symer <nate@natesymer.com>
2014-06-22 14:32:43 -0400

    Fixed call to super for Mystic::SQL::SpatialColumn::new

--- lib/mystic/sql.rb
+++ lib/mystic/sql.rb
@@ -77,8 +77,8 @@ module Mystic
       
       def initialize(opts={})
         @name = opts.delete(:name).to_s
-        @kind = opts.delete(:kind).to_sym
-        @size = opts.delete(:size).to_s
+        @kind = opts.delete(:kind).to_sym unless opts.member? :geom_kind
+        @size = opts.delete(:size).to_s if opts.member? :size
         @constraints = opts
       end
       
@@ -92,7 +92,7 @@ module Mystic
 										:geom_srid
       
       def initialize(opts={})
-        super opts
+        super #opts
         @geom_kind = opts[:geom_kind]
         @srid = opts[:geom_srid]
       end
